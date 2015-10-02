d39514564895079f5f6271ef2c18a8ef03b70ce2
zakihaya <toru.hayazaki@gmail.com>
2015-10-02 18:07:33 +0000

    Implementation of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,19 @@
 class MessagesController < ApplicationController
   def index
     @messages = Message.all
+    @message = Message.new
   end
 
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      # メッセージが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
   end
 
   private
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ActiveRecord::Base
+    # 名前は必須入力かつ20文字以内
+    validates :name , length: {  maximum: 20 } , presence: true
+    # 内容は必須入力かつ2文字以上30文字以下
+    validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
 end
