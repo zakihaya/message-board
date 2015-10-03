c1b2263b343664bff3ed18213d7db767cf1b17c8
zakihaya <toru.hayazaki@gmail.com>
2015-10-03 00:46:21 +0000

    Gemfile update for heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
+
   def index
     @messages = Message.all
     @message = Message.new
@@ -16,8 +18,25 @@ class MessagesController < ApplicationController
     end
   end
 
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
   private
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
